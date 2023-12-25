37468e7d7ef29f6b92d61afcad7b775ca9a4f854
sumanthssuvarna <sumanthssuvarna@gmail.com>
2023-12-25 10:03:57 +0530

    dec 25

---
+++ SQL/Ankit_Bansal/vid_1.sql
@@ -0,0 +1,14 @@
+ -- Complex SQL Query 1 | Derive Points table for ICC tournament
+
+ 
+ create table encouraging-key-348500.abc123.encouraging-key-348500.abc123.icc_world_cup
+(
+Team_1 string(20),
+Team_2 string(20),
+Winner string(20)
+);
+INSERT INTO encouraging-key-348500.abc123.icc_world_cup values('India','SL','India');
+INSERT INTO encouraging-key-348500.abc123.icc_world_cup values('SL','Aus','Aus');
+INSERT INTO encouraging-key-348500.abc123.icc_world_cup values('SA','Eng','Eng');
+INSERT INTO encouraging-key-348500.abc123.icc_world_cup values('Eng','NZ','NZ');
+INSERT INTO encouraging-key-348500.abc123.icc_world_cup values('Aus','India','India');
---
+++ SQL/abc.sql
@@ -0,0 +1,3 @@
+-- Creating an index on the 'salary' column
+CREATE INDEX idx_salary
+ON employees(salary);
