36e55ce981aa253606fa997ed20b75ab2e4f81cb
sumanthssuvarna <sumanthssuvarna@gmail.com>
2024-01-19 09:03:28 +0530

    from icc_world_cup

---
+++ Python/challenges/req_1.py
@@ -0,0 +1,2 @@
+# python code to read an excel data model and print the output into json file.
+# two files, columns -- filename,column_name,datatype 
--- SQL/Ankit_Bansal/icc_world_cup.sql
+++ SQL/Ankit_Bansal/icc_world_cup.sql
@@ -1,4 +1,5 @@
  -- Complex SQL Query 1 | Derive Points table for ICC tournament
+ -- https://www.youtube.com/watch?v=qyAgWL066Vo&list=PLBTZqjSKn0IeKBQDjLmzisazhqQy4iGkb
 
  
  create table encouraging-key-348500.abc123.encouraging-key-348500.abc123.icc_world_cup
@@ -12,3 +13,19 @@ INSERT INTO encouraging-key-348500.abc123.icc_world_cup values('SL','Aus','Aus')
 INSERT INTO encouraging-key-348500.abc123.icc_world_cup values('SA','Eng','Eng');
 INSERT INTO encouraging-key-348500.abc123.icc_world_cup values('Eng','NZ','NZ');
 INSERT INTO encouraging-key-348500.abc123.icc_world_cup values('Aus','India','India');
+
+select 
+team, 
+count(1) as total_matches,
+sum(result) as win ,
+count(1) - sum(result) as loose
+
+from
+(
+    select team_1 as team, (case when winner=team_1 then 1 else 0 end) as result from icc_world_cup
+    union all 
+    select team_2 as team, (case when winner=team_2 then 1 else 0 end) as result  from icc_world_cup
+) A
+
+group by team
+order by total_matches desc
